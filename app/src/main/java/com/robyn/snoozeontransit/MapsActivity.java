9283f754fcc0f62d933e23cb7693b6de7c5716e6
robyn <robyn3choi@gmail.com>
2015-07-31 16:41:14 -0700

    location may not update properly, but app opens (getbestprovider)

--- app/src/main/java/com/robyn/snoozeontransit/MapsActivity.java
+++ app/src/main/java/com/robyn/snoozeontransit/MapsActivity.java
@@ -5,10 +5,17 @@ import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -16,23 +23,76 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.io.IOException;
+
 public class MapsActivity extends FragmentActivity implements LocationListener, GoogleMap.OnMapClickListener {
 
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
     public LatLng destination;
     public LatLng current;
+    Location myLocation;
     public static final String TAG = MapsActivity.class.getSimpleName();
+    public float distance;
+
+    Button setAlarmButton;
+    Button stopAlarmButton;
+    Button cancelAlarmButton;
+    TextView resultText;
+
+    boolean isAlarmSet;
+    boolean isAlarmPlaying;
+
+    private MediaPlayer player;
+    final Context context = this;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
+        isAlarmSet = false;
+
+        setAlarmButton = (Button) findViewById(R.id.set_alarm_button);
+
+        setAlarmButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setAlarm();
+            }
+        });
+
+        stopAlarmButton = (Button) findViewById(R.id.stop_alarm_button);
+
+        stopAlarmButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stopAlarm();
+            }
+        });
+
+
+        cancelAlarmButton = (Button) findViewById(R.id.cancel_alarm_button);
+
+        cancelAlarmButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cancelAlarm();
+            }
+        });
+
+
+        resultText = (TextView) findViewById(R.id.result_text);
+
         setUpMapIfNeeded();
 
         if (mMap != null) {
             mMap.setMyLocationEnabled(true);
         }
     }
+
+
 
     @Override
     protected void onResume() {
@@ -89,10 +149,17 @@ public class MapsActivity extends FragmentActivity implements LocationListener, 
         String provider = locationManager.getBestProvider(criteria, true);
 
         // Get Current Location
-        Location myLocation = locationManager.getLastKnownLocation(provider);
+        myLocation = locationManager.getLastKnownLocation(provider);
 
-        // request that the provider send this activity GPS updates every 20 seconds
-        //locationManager.requestLocationUpdates(provider, 20000, 0, this);
+//         request that the provider send this activity GPS updates every 20 seconds
+        locationManager.requestLocationUpdates(provider, 5000, 0, this);
+
+//        if (locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
+//            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 5000, 0, this);
+//        } else {
+//            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 0,
+//                    this);
+//        }
 
         // set map type
         mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
@@ -119,10 +186,16 @@ public class MapsActivity extends FragmentActivity implements LocationListener, 
 
     @Override
     public void onLocationChanged(Location location) {
-        if (destination != null && current != null) {
-            calculateDistance(current, destination);
+
+        current = new LatLng(myLocation.getLatitude(), myLocation.getLongitude());
+
+        if (destination != null && myLocation != null) {
+            calculateDistance();
         }
 
+        if (isCloseEnough(distance) && isAlarmSet) {
+            playAlarm(this, getAlarmSound());
+        }
     }
 
 
@@ -143,37 +216,95 @@ public class MapsActivity extends FragmentActivity implements LocationListener, 
 
     @Override
     public void onMapClick(LatLng latLng) {
-        mMap.clear();
-        mMap.addMarker(new MarkerOptions()
-                .position(latLng)
-                .title("Destination")
-                .snippet("You're going here"));
-        destination = latLng;
+        if (!isAlarmSet) {                              // a new destination can't be created if there's already an alarm set
+            mMap.clear();
+            mMap.addMarker(new MarkerOptions()
+                    .position(latLng)
+                    .title("Destination")
+                    .snippet("You're going here"));
+            destination = latLng;
 
-        if (destination != null && current != null) {
-            calculateDistance(current, destination);
+            if (destination != null && current != null) {  // if there is a destination and a current
+                calculateDistance();
+            }
         }
     }
 
-    public float calculateDistance(LatLng curr, LatLng dest) {
-        float distance = 0;
-        double currLat = curr.latitude;
-        double currLong = curr.longitude;
-        double destLat = dest.latitude;
-        double destLong = dest.longitude;
+    public float calculateDistance() {
 
-        Location currLocation = new Location("currLocation");
-        currLocation.setLatitude(currLat);
-        currLocation.setLongitude(currLong);
+        double destLat = destination.latitude;
+        double destLong = destination.longitude;
+
+//        Location currLocation = new Location("currLocation");
+//        currLocation.setLatitude(currLat);
+//        currLocation.setLongitude(currLong);
 
         Location destLocation = new Location("destLocation");
         destLocation.setLatitude(destLat);
         destLocation.setLongitude(destLong);
 
-        distance = currLocation.distanceTo(destLocation) / 1000; // in km
+        distance = myLocation.distanceTo(destLocation) / 1000; // in km
+        double myLat = myLocation.getLatitude();
 
-        Log.d(TAG, "Distance is" + distance+"km.");
+  //      Log.d(TAG, "myLocation is" + myLat +"km.");
+        resultText.setText("You are " + Float.toString(distance) + "away from your destination. And myLat = " + String.valueOf(myLat) );
+
+
 
         return distance;
     }
+
+    public boolean isCloseEnough(float distance) {
+        return (distance <= 1);
+    }
+
+
+    public void setAlarm() {
+        isAlarmSet = true;
+
+    }
+
+    public void stopAlarm() {
+        if (isAlarmPlaying) {
+            player.stop();
+        }
+    }
+
+    public void cancelAlarm() {
+        isAlarmSet = false;
+    }
+
+    public void playAlarm(Context context, Uri alert) {
+        //STUB
+        if (isAlarmPlaying) {
+            return;
+        }
+        isAlarmPlaying = true;
+        player = new MediaPlayer();
+        try {
+            player.setDataSource(context, alert);
+            final AudioManager audio = (AudioManager) context
+                    .getSystemService(Context.AUDIO_SERVICE);
+            if (audio.getStreamVolume(AudioManager.STREAM_ALARM) != 0) {
+                player.setAudioStreamType(AudioManager.STREAM_ALARM);
+                player.prepare();
+                player.start();
+            }
+        } catch (IOException e) {
+         //   Log.e("Error....","Check code...");
+        }
+    }
+
+    //make the alarm sound the default alarm sound or ringtone or notification sound
+    private Uri getAlarmSound() {
+        Uri alertSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
+        if (alertSound == null) {
+            alertSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE);
+            if (alertSound == null) {
+                alertSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+            }
+        }
+        return alertSound;
+    }
+
 }
